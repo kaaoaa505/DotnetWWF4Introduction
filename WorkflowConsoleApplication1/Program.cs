4de0d82d75df2e6395d7229b791c6209503cf5e4
Khaled <kaaoaa505@gmail.com>
2021-10-30 17:21:30 +0200

    simple three types of windows workflow foundations

---
+++ WorkflowConsoleApplication1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Activities;
+using System.Activities.Statements;
+using System.Linq;
+
+namespace WorkflowConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Activity sequenceDelayActivity = new Workflow1();
+            WorkflowInvoker.Invoke(sequenceDelayActivity);
+
+            Console.WriteLine("----- Workflow 1 end.");
+
+            Activity flowchartActivity = new FlowChartWorkflow();
+            WorkflowInvoker.Invoke(flowchartActivity);
+
+            Console.WriteLine("----- Workflow 2 end.");
+
+            Activity stateMachineWorkflow = new StateMachineWorkflow();
+            WorkflowInvoker.Invoke(stateMachineWorkflow);
+
+            Console.WriteLine("----- Workflow 3 end.");
+
+            Console.ReadLine();
+        }
+    }
+}
